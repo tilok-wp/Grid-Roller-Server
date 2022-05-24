aa3d9e60604b3c3de0808d2c95529f8d5d10aa58
Tilok Paul <tilokpaul.web@gmail.com>
2022-05-25 02:35:18 +0600

    Create Admin API with verify method

---
+++ README.md
@@ -0,0 +1,50 @@
+# Grid Roller (BackEnd Server side)
+
+Best quality painting ROLLER manufacturer
+
+## live url: [https://grid-roller.web.app/](https://grid-roller.web.app/)
+
+---
+
+## Client side source code: [https://github.com/programming-hero-web-course1/manufacturer-website-client-side-tilok-wp.git](https://github.com/programming-hero-web-course1/manufacturer-website-client-side-tilok-wp.git)
+
+## Server side source Code:[https://github.com/programming-hero-web-course1/manufacturer-website-server-side-tilok-wp.git](https://github.com/programming-hero-web-course1/manufacturer-website-server-side-tilok-wp.git)
+
+## Application Features:
+
+- Modern and clean view
+- Add product
+- Delete product
+- Login authentication and authorization
+- Order lot quantity (Min but not over available)
+- Dashboard
+
+## Frameworks and libraries use in this App For Backend server side
+
+- Express
+- cors
+- mongodb
+- jsonwebtoken
+- stripe
+
+## Frameworks and libraries use in this App For front-end client side
+
+- React
+- CSS framework tailwind
+- CSS Component libraries Daisyui
+- Firebase Hosting for client side deploy
+- React toastify
+- React firebase hooks
+- React-helmet-async
+- React Router v6
+- Custom hooks
+- Firebase for authorization
+
+## Easy step to start this project localhost
+
+In the project directory, you can run from your local:
+
+### `npm run start-dev`
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const jwt = require('jsonwebtoken');
 
 
 require('dotenv').config()
@@ -16,7 +17,23 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_MONGO_NAME}:${process.env.DB_MONGO_PASS}@cluster0.2lwzs.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+// JWT verify
+const verifyJWT = (req, res, next) => {
+    const authHeader = req.headers.authorization
+    if (!authHeader) {
+        return res.status(401).send({ message: 'UnAuthorized access!' })
+    }
+    const token = authHeader.split(' ')[1]
 
+    jwt.verify(token, process.env.TOKEN_KEY_SECREATE, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access!' })
+        }
+        req.decoded = decoded
+        next()
+    });
+
+}
 
 // App API start here
 async function run() {
@@ -24,6 +41,17 @@ async function run() {
         await client.connect();
         const productCollection = client.db('grid_roller_database').collection('product')
         const blogCollection = client.db('grid_roller_database').collection('blog')
+        const userCollection = client.db('grid_roller_database').collection('user')
+
+        const verifyAdmin = async (req, res, next) => {
+            const requesterEmail = req.decoded.email
+            const requesterAccount = await userCollection.findOne({ email: requesterEmail })
+            if (requesterAccount.role === 'admin') {
+                next()
+            } else {
+                return res.status(403).send({ message: 'Forbidden access!' })
+            }
+        }
 
         app.get('/product', async (req, res) => {
             const limit = parseInt(req.query.limit)
@@ -37,12 +65,63 @@ async function run() {
             }
             res.send(products)
         })
+        // Add product post
+        app.post('/product', async (req, res) => {
+            const product = req.body
+            const productInserted = await blogCollection.insertOne(product)
+            res.send(productInserted)
+        })
 
-
+        // Get blog post
         app.get('/blog', async (req, res) => {
             const blogs = await blogCollection.find({}).toArray()
             res.send(blogs)
         })
+        // Add blog post
+        app.post('/blog', async (req, res) => {
+            const blog = req.body
+            const blogInserted = await blogCollection.insertOne(blog)
+            res.send(blogInserted)
+        })
+        // Create user and generate access token
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email
+            const user = req.body
+            // console.log(user)
+            const filter = { email: email }
+            const options = { upsert: true }
+            const updatedDoc = {
+                $set: user
+            }
+            const result = await userCollection.updateOne(filter, updatedDoc, options)
+            const token = jwt.sign({ email: email }, process.env.TOKEN_KEY_SECREATE, { expiresIn: '12h' })
+            // console.log(token)
+            res.send({ accessToken: token, result })
+        })
+
+        app.get('/user', verifyJWT, async (req, res) => {
+            const users = await userCollection.find().toArray()
+            res.send(users)
+        })
+        app.put('/user/admin/:email', verifyJWT, verifyAdmin, async (req, res) => {
+            const email = req.params.email
+            // console.log(req.params)
+            // const requesterEmail = req.decoded.email
+            // const requesterAccount = await userCollection.findOne({ email: requesterEmail })
+            if (requesterAccount.role === 'admin') {
+                const filter = { email: email }
+                const updatedDoc = {
+                    $set: { role: 'admin' }
+                }
+                const result = await userCollection.updateOne(filter, updatedDoc,)
+                res.send(result)
+            } else {
+                res.status(403).send({ message: 'Forbidden access!' })
+            }
+        })
+
+
+
 
     }
     finally { }
